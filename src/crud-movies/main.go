ebbfddc353eeb1eca50f25af8ae3ad491b7b0152
bardinGL <haonguyen20032003@gmail.com>
2024-03-08 01:09:02 +0700

    update the second time

---
+++ src/crud-movies/main.go
@@ -0,0 +1,32 @@
+package main
+
+import(
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"math/http"
+	"math/rand"
+	"strconv"
+)
+
+type Movie struct {
+	ID string 'json:"id"'
+	Isbn string 'json:"isbn"'
+	Title string 'json:"title"'
+	Director *Director 'json:"director"'
+}
+
+type Director struct {
+	Firstname string 'json:"firstname"'
+	Lastname string 'json:"lastname"'
+}
+
+var movies[] Movie
+
+func main() {
+	
+	r :=mux.NewRouter()
+
+	r.HandleFunc()
+}
